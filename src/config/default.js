96d121fa25d7fcc235206b9ca0af545d7bb2cd93
Eduardo Oliveira <santos.edu3301@gmail.com>
2022-07-25 07:57:57 -0300

    dynamically adding username to the paths in the config file

--- src/config/default.js
+++ src/config/default.js
@@ -1,5 +1,8 @@
+const os = require('os')
+const userName = os.userInfo().username
+
 module.exports = {
-  configPath: '/home/eduardo/.config',
-  phrasalVerbsPath: '/home/eduardo/Github/words-generator/src/files/phrasalVerbs.txt',
-  wordsPath: '/home/eduardo/Github/words-generator/src/files/words_alpha.txt'
+  configPath: `/home/${userName}/.config`,
+  phrasalVerbsPath: `/home/${userName}/Github/words-generator/src/files/phrasalVerbs.txt`,
+  wordsPath: `/home/${userName}/Github/words-generator/src/files/words_alpha.txt`
 }
